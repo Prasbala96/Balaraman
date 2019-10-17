96e09a6826ed698b0b4e6aeeaf86f9416627bcdd
Balaraman <bala95shan@gmail.com>
2019-10-17 20:28:39 +0530

    This Is First Commit

---
+++ eclipse-workspace/Satelite/src/com/cts/test/Lander.java
@@ -0,0 +1,11 @@
+package com.cts.test;
+
+public class Lander 
+{
+
+   public static void main(String[] args) 
+{
+	System.out.println("Hi This Is Lander");
+}
+
+}
---
+++ eclipse-workspace/Satelite/src/com/cts/test/Satelite.java
@@ -0,0 +1,11 @@
+package com.cts.test;
+
+public class Satelite 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi This Is Satelite");
+	}
+
+}
