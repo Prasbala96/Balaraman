08078fea380d35db7fa94e746ef5069f5f86b821
Balaraman <bala95shan@gmail.com>
2019-10-17 20:59:07 +0530

    Hi Lander

--- eclipse-workspace/Satelite/src/com/cts/test/Lander.java
+++ eclipse-workspace/Satelite/src/com/cts/test/Lander.java
@@ -3,9 +3,14 @@ package com.cts.test;
 public class Lander 
 {
 
+	public void sub()
+	{
+		System.out.println("Sub Method");
+	}
+	
    public static void main(String[] args) 
-{
-	System.out.println("Hi This Is Lander");
-}
+   {
+	   System.out.println("Hi This Is Lander");
+   }
 
 }
