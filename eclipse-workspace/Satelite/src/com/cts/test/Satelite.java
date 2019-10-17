c866e03e26bdd8e1656c5d16b4996db2ffc5e80c
Balaraman <bala95shan@gmail.com>
2019-10-17 20:54:14 +0530

    Second commit

--- eclipse-workspace/Satelite/src/com/cts/test/Satelite.java
+++ eclipse-workspace/Satelite/src/com/cts/test/Satelite.java
@@ -3,6 +3,11 @@ package com.cts.test;
 public class Satelite 
 {
 
+	public void add()
+	{
+		System.out.println("Added New Function");
+		
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hi This Is Satelite");
